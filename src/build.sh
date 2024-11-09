4e7d73dd4e1584e9ad6046c86820feb1a8283123
Paul W <lambdapaul@protonmail.com>
2024-11-09 00:23:49 -0500

    Bump sqlite version
    Signed-off-by: Paul W <lambdapaul@protonmail.com>

--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 
+OS="$(uname -o)"
+LIB_A="libsqlite3"
+
 pushd "$(dirname "$0")"
 
-ARCH="$(uname -m)"
-OS="$(uname -o)"
-LIB_A="libsqlite3_${ARCH}"
 
 if clang -v &> /dev/null; then
     CC=clang
@@ -17,14 +17,13 @@ else
 fi
 
 if [ $OS == "Darwin" ]; then
-    libtool -V &> /dev/null || { echo "Missing 'libtool'"; exit 2 }
+    libtool -V &> /dev/null || ( echo "Missing 'libtool'"; exit 2 )
+    $CC -c -O3 -Qn -arch x86_64 -o "x86_64.o" "./sqlite3.c" -DSQLITE_OMIT_DEPRECATED -DSQLITE_OMIT_UTF16 -DSQLITE_OMIT_TEST_CONTROL
+    libtool -static "x86_64.o" -o "../lib/${LIB_A}_x64.a"
+    $CC -c -O3 -Qn -arch arm64 -o "arm64.o" "./sqlite3.c" -DSQLITE_OMIT_DEPRECATED -DSQLITE_OMIT_UTF16 -DSQLITE_OMIT_TEST_CONTROL
+    libtool -static "arm64.o" -o "../lib/${LIB_A}_arm64.a"
 fi
 
-$CC -c -O3 -Qn "./sqlite3.c" -DSQLITE_OMIT_DEPRECATED -DSQLITE_OMIT_UTF16 -DSQLITE_OMIT_TEST_CONTROL
-
-if [ $OS == "Darwin" ]; then 
-    libtool -static "sqlite3.o" -o "../lib/${LIB_A}.a"
-fi
 
 rm *.o &> /dev/null
 rm *.tmp &> /dev/null
